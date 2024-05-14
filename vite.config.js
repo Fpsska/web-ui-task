c3915859f3c7c790a38d7a07c527670a5d08fc04
Fpsska <fpsska1337@gmail.com>
2024-05-14 21:05:52 +0300

    REFACTORING: overwrite logic of handle env variables in vite cfg, overwrite scripts in package cfg

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,58 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { resolve } from 'path';
 
 // /. imports
 
-const isProductionMode = process.env.NODE_ENV === 'production';
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const isProductionMode = env.VITE_APP_MODE === 'production';
+    return {
+        define: {
+            __APP_ENV__: JSON.stringify(env.APP_ENV)
+        },
+        base: isProductionMode ? '/web-ui-task' : '/',
+        server: {
+            port: '3000'
+        },
+        build: {
+            outDir: './build',
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'index.html'),
+                    products: resolve(__dirname, 'assets/pages/products.html'),
+                    failed_payment: resolve(
+                        __dirname,
+                        'assets/pages/failed_payment.html'
+                    ),
+                    successful_payment: resolve(
+                        __dirname,
+                        'assets/pages/successful_payment.html'
+                    )
+                },
+                output: {
+                    assetFileNames: ({ name }) => {
+                        if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+                            return 'assets/images/[name]-[hash][extname]';
+                        }
 
-export default defineConfig({
-    base: isProductionMode ? '/web-ui-task' : '/',
-    server: {
-        port: '3000'
-    },
-    build: {
-        outDir: './build',
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                products: resolve(__dirname, 'assets/pages/products.html'),
-                failed_payment: resolve(
-                    __dirname,
-                    'assets/pages/failed_payment.html'
-                ),
-                successful_payment: resolve(
-                    __dirname,
-                    'assets/pages/successful_payment.html'
-                )
-            },
-            output: {
-                assetFileNames: ({ name }) => {
-                    if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
-                        return 'assets/images/[name]-[hash][extname]';
+                        if (/\.(ttf|woff|woff2|eot)$/.test(name ?? '')) {
+                            return 'assets/fonts/[name]-[hash][extname]';
+                        }
+
+                        if (/\.css$/.test(name ?? '')) {
+                            return 'assets/css/[name]-[hash][extname]';
+                        }
+
+                        return 'assets/[name]-[hash][extname]';
                     }
-
-                    if (/\.(ttf|woff|woff2|eot)$/.test(name ?? '')) {
-                        return 'assets/fonts/[name]-[hash][extname]';
-                    }
-
-                    if (/\.css$/.test(name ?? '')) {
-                        return 'assets/css/[name]-[hash][extname]';
-                    }
-
-                    return 'assets/[name]-[hash][extname]';
                 }
             }
-        }
-    },
-    plugins: [ViteMinifyPlugin({})]
+        },
+        preview: {
+            outDir: './build'
+        },
+        plugins: [isProductionMode ? ViteMinifyPlugin({}) : undefined]
+    };
 });
